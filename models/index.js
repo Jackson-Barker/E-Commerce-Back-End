5e7930ed5a405ae8e85e81353d2f5ab19d7a8e28
jbarker <jackson.barker@du.edu>
2021-11-20 20:56:26 -0700

    models

--- models/index.js
+++ models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { getEnabledCategories } = require('trace_events');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete:'CASCADE',
+} )
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  through: ProductTag}, {
+  foreignKey: 'productTag_id',
+  });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag}, {
+    foreignKey: 'productTag_id',
+  });
+
 
 module.exports = {
   Product,
