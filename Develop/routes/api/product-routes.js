d5c0b11ac03df31e8e363d6dde18f512a9022ecb
jbarker <jackson.barker@du.edu>
2021-11-23 20:25:45 -0700

    it all works

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,14 +7,14 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  // try {
+  try {
     const productData = await Product.findAll({
-      include: [ Category, { model: Tag, through: ProductTag }],
+      include: [ Category, { model: Tag}],
     });
     res.status(200).json(productData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: ProductTag, include: [{model: Tag}] }],
+      include: [ Category, { model: Tag, through: ProductTag }],
     });
     if (!productData) {
       res.status(404).json({ message: "No product found with this id!" });
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -61,7 +61,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.delete({
+    const tagData = await Tag.destroy({
       where: {id: req.params.id}
     });
     if (!tagData) {
